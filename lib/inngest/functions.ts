1a14b698c0faec6a0f9c056c0d125dc8eaf222cb
debasish1452003 <debasishrana1452003@gmail.com>
2026-06-05 21:02:49 +0530

    cron correction

--- lib/inngest/functions.ts
+++ lib/inngest/functions.ts
@@ -62,8 +62,7 @@ export const sendSignUpEmail = inngest.createFunction(
 export const sendDailyNewsSummary = inngest.createFunction(
   {
     id: "daily-news-summary",
-    // triggers: [{ event: "app/send.daily.news" }, { cron: "0 12 * * *" }],
-    triggers: [{ event: "app/send.daily.news" }, { cron: "* * * * *" }],
+    triggers: [{ event: "app/send.daily.news" }, { cron: "0 12 * * *" }],
   },
 
   async ({ step }) => {
